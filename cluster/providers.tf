4405fd37e5196b125e20c248237796c7c018348a
Sohan Wadalkar <sohan.wadalkar@gmail.com>
2024-04-25 19:01:34 +0530

    commit1234

--- cluster/providers.tf
+++ cluster/providers.tf
@@ -9,7 +9,7 @@ terraform {
     resource_group_name  = "cloud-shell-storage-centralindia"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "csg10032003748e0302"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "terraform"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = ""        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = "tf"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
@@ -21,6 +21,6 @@ provider "azurerm" {
   subscription_id = var.subscriptionid
   tenant_id = var.tenantid
   features {
-    
+
   }
 }
