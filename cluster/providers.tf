eeda1df40e454f007665778764312ac1743e32d8
Sohan Wadalkar <sohan.wadalkar@gmail.com>
2024-04-25 19:31:04 +0530

    commit revert

--- cluster/providers.tf
+++ cluster/providers.tf
@@ -5,12 +5,12 @@ terraform {
       version = "=2.91.0"
     }
   }
-   backend "azurerm" {
-    resource_group_name  = "cloud-shell-storage-centralindia"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "csg10032003748e0302"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "terraform"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = ""        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
+  #  backend "azurerm" {
+  #   # resource_group_name  = "cloud-shell-storage-centralindia"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+  #   # storage_account_name = "csg10032003748e0302"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+  #   # container_name       = "terraform"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+  #   # key                  = ""        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  # }
 }
 
 # Configure the Microsoft Azure Provider
