a851d2508cbb248e92532a22c86e3d535cdcc395
Sohan Wadalkar <sohan.wadalkar@gmail.com>
2024-04-26 09:40:25 +0530

    vv

--- cluster/providers.tf
+++ cluster/providers.tf
@@ -10,6 +10,10 @@ terraform {
     storage_account_name = "csg10032003748e0302"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "terraform"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = ""        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    client_id = var.clientid
+  client_secret = var.clustersecrect
+  subscription_id = var.subscriptionid
+  tenant_id = var.tenantid
   }
 }
 
